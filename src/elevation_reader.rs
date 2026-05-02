f4dfc40acb49855d06fd6edc4304754df0e50f4f
vcoppe <vianney.coppe@gmail.com>
2026-05-02 14:02:23 +0200

    progress

--- src/elevation_reader.rs
+++ src/elevation_reader.rs
@@ -1,5 +1,6 @@
 use pmtiles::{AsyncPmTilesReader, MmapBackend, TileCoord};
 use webp::Decoder;
+use ndarray::Array2;
 
 pub struct ElevationReader {
     tile_size: usize,
@@ -14,14 +15,13 @@ impl ElevationReader {
         }
     }
 
-    pub async fn get(self, coord: TileCoord) -> Vec<f32> {
+    pub async fn get(self, coord: TileCoord) -> Array2<f64> {
         let bytes = self.reader.get_tile(coord).await.unwrap().unwrap();
         let decoder = Decoder::new(&bytes);
         let img = decoder.decode().unwrap();
-        let size = self.tile_size * self.tile_size;
-        let mut elevation = vec![0.0; size];
-        for i in 0..size {
-            elevation[i] = (img[3 * i] as f32) * 256.0 + img[3 * i + 1] as f32 + (img[3 * i + 2] as f32) / 256.0 - 32768 as f32;
+        let mut elevation = Array2::<f64>::zeros((self.tile_size, self.tile_size));
+        for i in 0..(self.tile_size * self.tile_size) {
+            elevation[[i / self.tile_size, i % self.tile_size]] = (img[3 * i] as f64) * 256.0 + img[3 * i + 1] as f64 + (img[3 * i + 2] as f64) / 256.0 - 32768 as f64;
         }
         elevation
     }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,26 @@ mod elevation_reader;
 
 use elevation_reader::ElevationReader;
 use pmtiles::TileCoord;
+use oxigdal_terrain::derivatives::hillshade::hillshade_traditional;
+use ndarray::Array2;
+use image::{ImageBuffer, Luma};
 
+fn save_array_as_png(data: &Array2<u8>, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let (height, width) = data.dim();
+    
+    // Convert Array2 to Vec in row-major order
+    let pixels: Vec<u8> = data
+        .iter()
+        .copied()
+        .collect();
+    
+    // Create ImageBuffer (Luma = grayscale)
+    let img = ImageBuffer::<Luma<u8>, Vec<u8>>::from_raw(width as u32, height as u32, pixels)
+        .ok_or("Failed to create image buffer")?;
+    
+    img.save(filename)?;
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
@@ -11,5 +30,8 @@ async fn main() {
     let reader = ElevationReader::new(file, tile_size).await;
     
     let coord = TileCoord::new(12, 2078, 1554).unwrap();
-    let data = reader.get(coord).await;
+    let elevation = reader.get(coord).await;
+    let hillshade = hillshade_traditional(&elevation, 1.0, 215.0, 45.0, 1.0, None).unwrap();
+    save_array_as_png(&hillshade, "hillshade.png");
+    println!("{:?}", hillshade);
 }

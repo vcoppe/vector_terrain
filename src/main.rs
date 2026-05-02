6a3f87e5a106bf1786b283a9fb641dd31f99e85c
vcoppe <vianney.coppe@gmail.com>
2026-05-02 18:28:14 +0200

    package change

--- src/main.rs
+++ src/main.rs
@@ -7,33 +7,48 @@ use pmtiles::TileCoord;
 use oxigdal_core::buffer::RasterBuffer;
 use image::{ImageBuffer, Luma};
 use oxigdal_algorithms::raster::swiss_hillshade;
-use oxigdal_algorithms::raster::polygonize::{polygonize_raster, PolygonizeOptions};
+use contour::ContourBuilder;
 
 const TILE_SIZE: usize = 512;
+// const THRESHOLDS:  [f64; 5] = [0.0, 96.0, 112.0, 140.0, 256.0];
+const THRESHOLDS:  [f64; 4] = [96.0, 112.0, 140.0, 256.0];
+const COLORS:  [f64; 5] = [256.0, 256.0, 224.0, 160.0, 128.0];
+const CLASS_NAMES:  [&str; 3] = ["0", "1", "2"];
 
-fn save_array_as_png(data: &RasterBuffer, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
+fn get_class(value: f64) -> f64 {
+    for (i, t) in THRESHOLDS.iter().enumerate() {
+        if value <= *t {
+            return COLORS[i];
+        }
+    }
+    0.0
+}
+
+fn save_buffer_as_png(data: &RasterBuffer, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
     let mut pixels = vec![0; TILE_SIZE * TILE_SIZE];
     for i in 0..TILE_SIZE {
         for j in 0..TILE_SIZE {
             pixels[i * TILE_SIZE + j] = data.get_pixel(i as u64, j as u64).unwrap() as u8;
         }
     }
+    as_png(pixels, filename)
+}
+
+fn save_array_as_png(data: &Vec<f64>, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut pixels = vec![0; TILE_SIZE * TILE_SIZE];
+    for i in 0..TILE_SIZE {
+        for j in 0..TILE_SIZE {
+            pixels[i * TILE_SIZE + j] = data[i * TILE_SIZE + j] as u8;
+        }
+    }
+    as_png(pixels, filename)
+}
+
+fn as_png(pixels: Vec<u8>, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
     let img = ImageBuffer::<Luma<u8>, Vec<u8>>::from_raw(TILE_SIZE as u32, TILE_SIZE as u32, pixels)
         .ok_or("Failed to create image buffer")?;
     img.save(filename)?;
     Ok(())
-}
-
-fn get_class(hillshade: f64) -> f64 {
-    if hillshade <= 128.0 {
-        128.0
-    } else if hillshade <= 144.0 {
-        160.0
-    } else if hillshade <= 160.0 {
-        224.0
-    } else {
-        256.0
-    }
 }
 
 #[tokio::main]
@@ -44,18 +59,24 @@ async fn main() {
     let coord = TileCoord::new(12, 2078, 1554).unwrap();
     let elevation = reader.get(coord).await;
     let mut hillshade = swiss_hillshade(&elevation, 1.0, 30.0).unwrap();
-    save_array_as_png(&hillshade, "hillshade.png");
-    
+    save_buffer_as_png(&hillshade, "hillshade.png");
+
     for i in 0..TILE_SIZE {
         for j in 0..TILE_SIZE {
-            hillshade.set_pixel(i as u64, j as u64, get_class(hillshade.get_pixel(i as u64, j as u64).unwrap()));
+            hillshade.set_pixel(i as u64, j as u64, 255.0 - hillshade.get_pixel(i as u64, j as u64).unwrap());
         }
     }
-    save_array_as_png(&hillshade, "classes.png");
 
-    let mut opts = PolygonizeOptions::default();
-    opts.nodata = Some(256.0);
-    opts.min_area = 10.0;
-    let result = polygonize_raster(&hillshade, &opts).unwrap();
-    TileEncoder::encode(TILE_SIZE, coord, &result.polygons);
+    // let mut classes = vec![0.0; TILE_SIZE * TILE_SIZE];
+    // for i in 0..TILE_SIZE {
+    //     for j in 0..TILE_SIZE {
+    //         classes[i * TILE_SIZE + j] = get_class(hillshade.get_pixel(i as u64, j as u64).unwrap());
+    //     }
+    // }
+    // save_array_as_png(&classes, "classes.png");
+    
+    let c = ContourBuilder::new(TILE_SIZE, TILE_SIZE, false);
+    let bands = c.isobands(hillshade.as_slice().unwrap(), &THRESHOLDS).unwrap();
+
+    TileEncoder::encode(TILE_SIZE, coord, &bands, &CLASS_NAMES);
 }
--- src/tile_encoder.rs
+++ src/tile_encoder.rs
@@ -1,32 +1,36 @@
-use oxigdal_algorithms::raster::polygonize::PolygonFeature;
 use mvt::{Error, GeomEncoder, GeomType, Tile};
 use pmtiles::{PmTilesWriter, TileType, TileCoord};
 use std::fs::File;
+use contour::Band;
 
 pub struct TileEncoder {}
 
 impl TileEncoder {
 
-    pub fn encode(tile_size: usize, tile_coord: TileCoord, polygons: &Vec<PolygonFeature>) -> Result<(), Error> {
+    pub fn encode(tile_size: usize, tile_coord: TileCoord, bands: &Vec<Band>, names: &[&str; 3]) -> Result<(), Error> {
         let mut tile = Tile::new(tile_size as u32);
         let mut layer = tile.create_layer("hillshading");
 
-        for polygon_feature in polygons {
-            let mut b = GeomEncoder::new(GeomType::Polygon);
-            for coord in polygon_feature.polygon.exterior().coords() {
-                b = b.point(coord.y, coord.x)?;
-            }
-            b.complete_geom()?;
-            for interior in polygon_feature.polygon.interiors() {
-                for coord in interior.coords() {
+        for band in bands.iter() {
+            let intensity_val = band.max_v().to_string();
+            println!("{} {} {}", band.min_v(), band.max_v(), intensity_val);
+            for polygon in band.geometry().iter() {
+                let mut b = GeomEncoder::new(GeomType::Polygon);
+                for coord in polygon.exterior().coords() {
                     b = b.point(coord.y, coord.x)?;
                 }
                 b.complete_geom()?;
+                for interior in polygon.interiors() {
+                    for coord in interior.coords() {
+                        b = b.point(coord.y, coord.x)?;
+                    }
+                    b.complete_geom()?;
+                }
+                let data = b.encode()?;
+                let mut feature = layer.into_feature(data);
+                feature.add_tag_string("intensity", intensity_val.as_str());
+                layer = feature.into_layer();
             }
-            let data = b.encode()?;
-            let mut feature = layer.into_feature(data);
-            feature.add_tag_string("intensity", "todo");
-            layer = feature.into_layer();
         }
         tile.add_layer(layer)?;
         let data = tile.to_bytes()?;
